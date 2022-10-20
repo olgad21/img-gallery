69d3bec176949b793f13cc710882ee1a46092bc8
Olga <olga.dashuk21@gmail.com>
2022-10-20 15:21:02 -0400

    feat: cadd hooks to home component

--- my-app/src/routes/home/home.component.tsx
+++ my-app/src/routes/home/home.component.tsx
@@ -1,4 +1,4 @@
-import React, { Component, KeyboardEventHandler } from "react";
+import React, { KeyboardEventHandler, useEffect, useState } from "react";
 import CardList from "../../components/card-list/card-list.component";
 import SearchBar from "../../components/search-bar/search-bar.component";
 import { getData } from "../../utils/data.utils";
@@ -12,58 +12,59 @@ export type HomeState = {
   isLoading: boolean;
 };
 
-class Home extends Component<{}, HomeState> {
-  state: HomeState = {
-    photos: [],
-    searchValue: localStorage.getItem("search") || "",
-    isLoading: false,
+const Home = () => {
+  const [searchValue, setSearchValue] = useState(
+    localStorage.getItem("search") || ""
+  );
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", saveToStorage);
+    fetchUsers(searchValue);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      saveToStorage();
+      window.removeEventListener("beforeunload", saveToStorage);
+    };
+  });
+
+  const saveToStorage = () => {
+    localStorage.setItem("search", searchValue);
   };
 
-  componentDidMount() {
-    window.addEventListener("beforeunload", this.saveToStorage);
-    this.fetchUsers(this.state.searchValue);
-  }
+  const onSearchChange: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.key === "Enter") {
+      setSearchValue((event.target as HTMLInputElement).value);
+      fetchUsers((event.target as HTMLInputElement).value);
+    }
+  };
 
-  componentWillUnmount() {
-    this.saveToStorage();
-    window.removeEventListener("beforeunload", this.saveToStorage);
-  }
-
-  fetchUsers = async (searchedText: string) => {
-    this.setState({ isLoading: true });
+  const fetchUsers = async (searchedText: string) => {
+    setIsLoading(true);
     const response = await getData<FlickrResponse>(
       `${host}&api_key=${apiKey}&text=${searchedText}&per_page=100&page=1&format=json&nojsoncallback=1`
     );
     const photos = response.photos.photo;
-    this.setState({ photos: photos, isLoading: false });
+
+    setPhotos(photos);
+    setIsLoading(false);
   };
 
-  saveToStorage = () => {
-    localStorage.setItem("search", this.state.searchValue);
-  };
-
-  onSearchChange: KeyboardEventHandler<HTMLInputElement> = (event) => {
-    if (event.key === "Enter") {
-      this.setState({ searchValue: (event.target as HTMLInputElement).value });
-      this.fetchUsers((event.target as HTMLInputElement).value);
-    }
-  };
-
-  render() {
-    const { photos, searchValue, isLoading } = this.state;
-    return (
-      <div className="App" id="home">
-        <h1 className="app-title">Image Gallery</h1>
-        <SearchBar
-          onChangeHandler={this.onSearchChange}
-          className="monsters-search-box"
-          defaultValue={searchValue}
-        />
-        {isLoading && <DownloadMessage />}
-        <CardList photos={photos} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App" id="home">
+      <h1 className="app-title">Image Gallery</h1>
+      <SearchBar
+        onChangeHandler={onSearchChange}
+        className="monsters-search-box"
+        defaultValue={searchValue}
+      />
+      {isLoading && <DownloadMessage />}
+      <CardList photos={photos} />
+    </div>
+  );
+};
 
 export default Home;
