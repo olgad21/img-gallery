8ffb4c7ae3b956a4d1c96c54186463b398308140
Olga <olga.dashuk21@gmail.com>
2023-01-09 18:42:22 -0500

    fix: change homepage url

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/olgad21-REACT2022Q3">
+      <BrowserRouter basename="/img-gallery">
         <App />
       </BrowserRouter>
     </Provider>
